00a075451b43fd63c5aa6c70f545cef0ca6216ed
nova1113 <nova9353@gmail.com>
2021-12-14 23:18:48 +0900

    Hash Problem 2

---
+++ src/Java/HashSecond.java
@@ -0,0 +1,36 @@
+package Java;
+
+import java.util.*;
+
+public class HashSecond {
+
+    public static void main(String[] args) {
+
+        String[] phone_book = {"119", "97674223", "1195524421"};
+//        String[] phone_book = {"123","456","789"};
+//        String[] phone_book = {"12","123","1235","567","88"};
+
+        System.out.println(solution(phone_book));
+    }
+
+    public static boolean solution(String[] phone_book){
+        boolean answer = true;
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for(String p : phone_book){
+            map.put(p, p.length());
+        }
+
+        for(String key : map.keySet()){
+            for(int i = 1; i < key.length() ; i++){
+                if (map.containsKey(key.substring(0, i))){
+                    return false;
+                }
+            }
+        }
+
+
+
+        return answer;
+    }
+}
